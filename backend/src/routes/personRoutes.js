1ea894f19ffd5de2c730dc4a9f5e257512562eff
beppe-saltini <beppe.saltini@gmail.com>
2025-06-12 12:03:38 +0100

    Update personRoutes.js

--- backend/src/routes/personRoutes.js
+++ backend/src/routes/personRoutes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const personController = require('../controllers/personController');
 
+const router = express.Router();
+
+// GET routes
+router.get('/', personController.getAllPersons.bind(personController));
+router.get('/stats', personController.getPersonStats.bind(personController));
+router.get('/birthdays', personController.getUpcomingBirthdays.bind(personController));
+router.get('/relationship/:relationship', personController.getPersonsByRelationship.bind(personController));
+
+// POST routes
+router.post('/', personController.addPerson.bind(personController));
+
+module.exports = router;
